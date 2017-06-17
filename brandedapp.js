06b0a59921d4343339e6f93a164183e3087582e3
Rory Goodman <goodmanr@tcd.ie>
2017-06-17 22:40:50 +0100

    Added carousel showing features of the app using slick carousel

--- brandedapp.js
+++ brandedapp.js
@@ -1,25 +1,54 @@
 
 var info = ["Discover and connect with the right people to promote your brand",
  "Personalised profile and integrated social media.You can now rate your top influencers",
-  "Beautiful grid listings, discover all your influencersin one place at any time",
+  "Beautiful grid listings, discover all your influencers in one place at any time",
   "Formatted menu, explore our application with ease"];
 var i=0;
 var iterations=1;
-document.getElementById('infotext').innerHTML=info[i];
 function next(n){
 	if(i>=n-1){
-		return ;
+		i=0 ;
 	}
-	i++;
+	var infotext = document.getElementById("infotext");
+	var topbar = document.getElementById("topbar");
+	var bottombar = document.getElementById("bottombar");
 	iterations++;
-	document.getElementById('infotext').style.animationIterations = iterations;
-	setTimeout(function() {
-		document.getElementById('infotext').innerHTML=info[i];
-		iterations++;
-		document.getElementById('infotext').style.animationIterations= iterations;
-	}, 2000);
-	
-	
+	fadeText(infotext);
+	closeBars(topbar,bottombar);
+	i++;
+	setTimeout(function(){
+		infotext.innerHTML = info[i];
+		iterations++;	
+		unFadeText(infotext);
+		openBars(topbar,bottombar);
+	},1200)
+	console.log(iterations);
+	return;
+}
+function fadeText(infotext){
+	infotext.style.animationDirection = "reverse";
+	infotext.style.animationIterationCount = iterations;
+	infotext.webkitAnimationIterationCount = iterations;
+	return;
+}
+function unFadeText(infotext){
+	infotext.style.animationDirection = "normal";
+	infotext.style.animationIterationCount = iterations;
+	infotext.style.webkitAnimationIterationCount = iterations;
+	return;
+}
+function closeBars(topbar,bottombar){
+	topbar.style.animationDirection = "reverse";
+	bottombar.style.animationDirection = "reverse";
+	topbar.style.animationIterationCount = iterations;
+	bottombar.style.animationIterationCount = iterations;
+	return;
+}
+function openBars(topbar,bottombar){
+	topbar.style.animationDirection = "normal";
+	bottombar.style.animationDirection = "normal";
+	topbar.style.animationIterationCount = iterations;
+	bottombar.style.animationIterationCount = iterations;
 	return;
 }
 function prev(n){
@@ -28,7 +57,4 @@ function prev(n){
 	}
 	i--;
 	return;
-}
-function infotext(){
-	return info[i];
 }
